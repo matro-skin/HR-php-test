62b5ce388c907ca214ac081bc2b86a4c3f869728
psprokofiev <ps.prokofiev@gmail.com>
2020-03-11 12:40:54 +0400

    add custom js/css

---
+++ resources/assets/js/script.js
@@ -0,0 +1,28 @@
+function processAjaxData(response, urlPath){
+	document.getElementById("content").innerHTML = response.html;
+	document.title = response.pageTitle;
+	window.history.pushState({"html":response.html,"pageTitle":response.pageTitle},"", urlPath);
+}
+
+function loadOrders(path) {
+	window.history.pushState(null,'', path);
+	$('#orderTabs').addClass('loading');
+	axios.get(path)
+		.then(response => {
+			$('#content').html( response.data.data );
+		})
+		.finally(() => {
+			$('#orderTabs').removeClass('loading');
+		})
+}
+
+$(document).on('show.bs.tab', '#ordersTab', function (e) {
+	let path = $(e.target).attr('href');
+	loadOrders(path);
+});
+
+$(document).on('click', '#orderTabs a.page-link', function (e) {
+	e.preventDefault();
+	let path = $(this).attr('href');
+	loadOrders(path);
+});
