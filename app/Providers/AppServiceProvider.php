03bcb4aafe3fafa4e8c073e439a95e0e92f747f0
psprokofiev <ps.prokofiev@gmail.com>
2020-03-11 12:41:35 +0400

    add collection pagination to AppServiceProdvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,14 @@
 namespace App\Providers;
 
 use App\Services\YandexWeather;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+
     /**
      * Bootstrap any application services.
      *
@@ -14,7 +18,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
+    	// Collection pagination
+	    if ( ! Collection::hasMacro('paginate') ) {
+		    Collection::macro('paginate',
+			    function ($perPage = 25, $page = null, $options = []) {
+				    $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+				    return (new LengthAwarePaginator(
+					    $this->forPage($page, $perPage), $this->count(), $perPage, $page, $options))
+					    ->withPath('');
+			    });
+	    }
+
+	    \App\Order::observe(\App\Observers\OrderObserver::class );
     }
 
     /**
@@ -26,4 +42,5 @@ class AppServiceProvider extends ServiceProvider
     {
 	    $this->app->bind(YandexWeather::class);
     }
+
 }
